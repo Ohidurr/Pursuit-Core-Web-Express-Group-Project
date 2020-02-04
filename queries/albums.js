7a94170bb00621895d956e8664da68ba32472a84
Kelvin <kelvinarellano@pursuit.org>
2020-02-03 21:56:48 -0500

    albums query made

--- queries/albums.js
+++ queries/albums.js
@@ -13,4 +13,45 @@ const getAlbums = async (req, res, next) => {
     }
 }
 
-module.exports = {getAlbums}
+const getAlbum = async(req, res, next) => {
+    try{
+        let album = await db.one (
+            "SELECT * FROM albums WHERE id = 1", req.params.id
+        )
+
+        res.status(200).json({
+            status: "Success",
+            message: "Pull one album",
+            body: album
+            
+        })
+    }catch(err) {
+        next(err)
+    }
+}
+
+const createAlbum = async(req, res, next) => {
+    try{
+        await db.none(`INSERT INTO albums (creator_id, album_title) VALUES ${creator_id}, ${album_title}) RETURNING *`, req.body)
+        res.status(200).json({
+            status: "Success",
+            message:"New Album Created"
+        })
+    }catch(err) {
+        next(err)
+    }
+}
+
+const deleteAlbum = async (req, res, next) => {
+    try{
+        await db.none(`DELETE FROM albums WHERE id = $1`, req.params.id)
+        res.status(200).json({
+            status:"Success",
+            message: album + "deleted"
+        })
+    }catch(err){
+        next(err)
+    }
+}
+
+module.exports = {getAlbums, getAlbum, createAlbum, deleteAlbum}
