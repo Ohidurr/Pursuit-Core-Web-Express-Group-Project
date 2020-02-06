9d108d4c7daa67edf67b02177e6ca43efd67c952
karenm09 <karenm929@hotmail.com>
2020-02-06 12:28:26 -0500

    update on the comments for each endpoint

--- routes/posts/posts.js
+++ routes/posts/posts.js
@@ -3,13 +3,13 @@ const posts = require("express").Router();
 const { getPosts, getPost, createPost, deletePost, getAllCommentsByPost } = require("../../queries/posts");
 
 
-//http://localhost:3000/posts/posts/comments/1
+//http://localhost:3000/posts/comments/1
 posts.get("/comments/:id", getAllCommentsByPost)
 
 //http://localhost:3000/posts/home
 posts.get("/home", getPosts);
 
-//http://localhost:3000/posts/1 == error
+//http://localhost:3000/posts/1
 posts.get("/:id", getPost);
 
 posts.post("/", createPost);
