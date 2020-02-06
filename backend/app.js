0a4b723d53dedf394122d2472fcb046c425f6337
karenm09 <karenm929@hotmail.com>
2020-02-06 10:32:06 -0500

    remove the postCommentR router and replace with commentsRouter

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,14 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const port = 3000;
 const app = express();
-const postsRouter = require("../routes/posts/posts.js")
-const postsCommentsR = require("../routes/comments/comments")
+const postsRouter = require("../routes/posts/posts.js");
+const commentsRouter = require("../routes/comments/comments");
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use("/posts", postsRouter);
-app.use("/posts", postsCommentsR);
+app.use("/comments", commentsRouter);
 
 
 app.listen(port, ()=> {
