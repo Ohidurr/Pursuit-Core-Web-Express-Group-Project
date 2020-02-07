0ffef2672545b4095e676295d9944014c1df069e
karenm09 <karenm929@hotmail.com>
2020-02-06 20:52:59 -0500

    deleted comments folder within posts folder

--- routes/posts/comments/comments.js
+++
@@ -1,7 +0,0 @@
-// const postsComments = require("express").Router({mergeParams:true});
-
-// const { getAllCommentsByPost } = require("../../../queries/posts")
-
-// postsComments.get("/:id", getAllCommentsByPost)
-
-// module.exports = postsComments;
