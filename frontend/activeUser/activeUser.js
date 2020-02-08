d00c3d1c33386dd1752a2dbc26eada9ae53d5c29
karenm09 <karenm929@hotmail.com>
2020-02-07 20:45:22 -0500

    add the getAllAlbums function, remove the elements from the css and build it through the js file

--- frontend/activeUser/activeUser.js
+++ frontend/activeUser/activeUser.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
    getAllAlbums()
+   // getAlbum()
    
 })
 
@@ -8,21 +9,34 @@ const getAllAlbums = async () => {
    try {
       let res = await axios.get(`http://localhost:3000/posts/home`)
       let users = res.data.body
-
+      
       users.forEach(user => {
          let container = document.querySelector("#container")
          let picFrame = document.createElement("div")
          picFrame.className = "responsive";
          let album = document.createElement("div");
          album.className = "gallery"
+
          let img = document.createElement("img");
          let h3 = document.createElement("h3");
          h3.className = "username"
+
          let caption = document.createElement("div");
-         caption.className = "desc"
+         caption.className = "desc";
+
          let p2 = document.createElement("p");
 
-         img.src = user.array_agg[0]
+         pics = user.array_agg
+         
+         // img.addEventListener("click", ()=>{
+            pics.forEach(pic => {
+               let ul = document.createElement("ul")
+               let li = document.createElement("li")
+               img.src = pic
+            })
+      
+         // })
+         
          h3.innerText = user.username
          p2.innerText = user.body
          album.appendChild(img)
@@ -31,8 +45,11 @@ const getAllAlbums = async () => {
          picFrame.append(album, caption)
          container.appendChild(picFrame)
       })
-      getAlbum(username)
    } catch (err) {
         console.log(err)
    }
 }
+
+// const getAlbum = () => {
+//    console.log("hey")
+// }
