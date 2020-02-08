59881478c53bc464a394a88a1eb822917c484236
Kelvin <kelvinarellano@pursuit.org>
2020-02-08 07:25:17 -0500

    log js updated

--- frontend/login/login.js
+++ frontend/login/login.js
@@ -1,7 +1,17 @@
 document.addEventListener("DOMContentLoaded", () =>{
   let loginButton = document.querySelector("#logInBtn");
   let loginForm = document.querySelector("#loginForm");
-  loginButton.addEventListener("click", ()=> {
-    
-  })
+  loginButton.addEventListener("click", userCheck);
 })
+
+const noUser = () => {
+  clearP()
+  const p = document.querySelector("p");
+  p.innerText = "No User With That Information. Click Sign up.";
+}
+
+const userCheck = async () => {
+  const userName = document.querySelector("#userLoginInput").value;
+  const password = document.querySelector("#loginPasswordInput").value;
+  const res = await axios.get("http://")
+}
